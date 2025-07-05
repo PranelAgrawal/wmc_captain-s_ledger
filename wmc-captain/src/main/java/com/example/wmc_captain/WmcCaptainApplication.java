cd810aba20a35d8e0a97a4994a2e7e72ed55a69f
PranelAgrawal <pranel.a@ahduni.edu.in>
2025-07-05 18:41:12 +0530

    pranel

---
+++ wmc-captain/src/main/java/com/example/wmc_captain/WmcCaptainApplication.java
@@ -0,0 +1,13 @@
+package com.example.wmc_captain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmcCaptainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WmcCaptainApplication.class, args);
+	}
+
+}
---
+++ wmc-captain/src/test/java/com/example/wmc_captain/WmcCaptainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wmc_captain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmcCaptainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
